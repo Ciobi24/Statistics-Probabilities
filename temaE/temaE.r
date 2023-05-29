8cc6ff5128c39bbd3b67d1fab5779b1701d41d92
Andra Ciobanu <119164551+Ciobi24@users.noreply.github.com>
2023-05-29 15:46:20 +0300

    Update temaE.r

--- temaE/temaE.r
+++ temaE/temaE.r
@@ -14,35 +14,33 @@ zconfidence_interval(1 - 0.99, 10, 138, 11)
 zconfidence_interval(1 - 0.95, 256, 18, sqrt(1.44))
 
 #III
-schimbare = function(lvl, n, succes, p0, tip_ip) {
-  p_prim = succes / n
-  z_score = (p_prim - p0) / sqrt(p0 * (1 - p0) / n)
-  cat(z_score, "\n")
-  if (tip_ip == "r") {
-    critical_z = qnorm(1 - lvl, 0, 1)
-    cat(critical_z, "\n")
-    if (z_score > critical_z) {
-      cat("we reject H0\n")
-    } else {
-      cat("we don't have enough evidence to reject H0\n")
-    }
-  } else if (tip_ip == "l") {
-    critical_z = qnorm(1 - lvl, 0, 1)
-    cat(critical_z, "\n")
-    if (z_score < critical_z) {
-      cat("we reject H0\n")
-    } else {
-      cat("we don't have enough evidence to reject H0\n")
-    }
-  } else {
-    critical_z = qnorm(1 - lvl / 2, 0, 1)
-    cat(critical_z, "\n")
-    if (abs(z_score) > critical_z) {
-      cat("we reject H0\n")
-    } else {
-      cat("we don't have enough evidence to reject H0\n")
-    }
+test_proport = function(alfa, n, succese, p0, tip_ip) {
+  p_prim = succese/n;
+  z_score = (p_prim - p0)/sqrt(p0*(1-p0)/n)
+  if(tip_ip == "r"){
+    critical_z = qnorm(1-alfa, 0, 1)
+    cat("critical_z=", critical_z)
+    if(z_score <= critical_z)
+      print("H0 nu se poate respinge")
+    else
+      print("H0 se respinge")
+  }
+  if(tip_ip == "l") {
+    critical_z = qnorm(alfa, 0, 1)
+    cat("critical_z=", critical_z)
+    if(z_score <= critical_z)
+      print("H0 nu se poate respinge")
+    else
+      print("H0 se respinge")
+  }
+  if(tip_ip == "s") {
+    critical_z = qnorm(1-alfa/2, 0, 1)
+    cat("critical_z=", critical_z)
+    if(abs(z_score) <= critical_z)
+      print("H0 nu se poate respinge")
+    else
+      print("H0 se respinge")
   }
 }
-schimbare(0.01, 153, 17, 0.12, "r")
-schimbare(0.05, 153, 17, 0.12, "r")
+test_proport(0.01, 153, 17, 0.12, "r")
+test_proport(0.05, 153, 17, 0.12, "r")
